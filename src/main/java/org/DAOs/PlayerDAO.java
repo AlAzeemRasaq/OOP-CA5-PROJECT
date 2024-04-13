f6af95b5a2117ee7dca78eb2e7be9df9d5123164
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-04-13 19:47:09 +0100

    Add files via upload

---
+++ src/main/java/org/DAOs/PlayerDAO.java
@@ -0,0 +1,16 @@
+package org.DAOs;
+
+import org.DTOs.PlayerDTO;
+import org.Exceptions.DaoException;
+
+import java.util.List;
+
+public interface PlayerDAO {
+    List<PlayerDTO> getAllPlayers() throws DaoException;
+    PlayerDTO getPlayerById(int playerId) throws DaoException;
+    void insertPlayer(PlayerDTO player) throws DaoException;
+    void updatePlayer(PlayerDTO player) throws DaoException;
+    void deletePlayer(int playerId) throws DaoException;
+    void findPlayerUsingFilter(PlayerDTO player) throws DaoException;
+    void playersListToJson(PlayerDTO player) throws DaoException;
+}
